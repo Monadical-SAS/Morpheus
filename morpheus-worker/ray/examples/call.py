14f8d19e4f94fe1ac265b5ddb809090b594c1108
Angel Rey <angel@monadical.com>
2023-07-19 11:39:09 -0500

    added

---
+++ morpheus-worker/ray/examples/call.py
@@ -0,0 +1,11 @@
+from ray.job_submission import JobSubmissionClient
+
+# If using a remote cluster, replace 127.0.0.1 with the head node's IP address.
+client = JobSubmissionClient("http://127.0.0.1:8265")
+job_id = client.submit_job(
+    # Entrypoint shell command to execute
+    entrypoint="python test-cu.py",
+    # Path to the local directory that contains the script.py file
+    runtime_env={"working_dir": "./", "env_vars": {"PROMPT": "cucu"}}
+)
+print(job_id)
