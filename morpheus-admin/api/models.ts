ef75d805b84d3fdbbfa4bc11bf81c0ff613adefe
Alejandro Sanchez Yali <asanchezyali@gmail.com>
2023-09-12 15:49:46 -0500

    add createResponse function

--- morpheus-admin/api/models.ts
+++ morpheus-admin/api/models.ts
@@ -1,19 +1,35 @@
 import httpInstance from "./axiosClient";
 import { Model } from "@/lib/models";
 
+interface ResponseProps {
+  success: boolean;
+  data?: any[];
+  message?: object | string | Error;
+}
+
+const createResponse = ({ success, data = [], message }: ResponseProps) => {
+  const responseMap: { [key: string]: { success: boolean; message: string; data: any[] } } = {
+    Error: { success: false, message: message instanceof Error ? message.message : "", data },
+    string: { success, message: typeof message === "string" ? message : "", data },
+    object: { success, message: typeof message === "object" ? JSON.stringify(message) : "", data },
+    default: { success, message: "", data },
+  };
+  return responseMap[typeof message] || responseMap["default"];
+};
+
 export const getAvailableModels = async () => {
   try {
     const response = await httpInstance.get("/models");
     if (response.status === 200 && response.data.length > 0) {
-      return { success: true, data: response.data };
+      return createResponse({ success: true, data: response.data });
     }
-    return {
+    return createResponse({
       success: false,
       message: response.data.message || "No models found",
       data: [],
-    };
+    });
   } catch (error) {
-    return { success: false, message: error, data: [] };
+    return createResponse({ success: false, message: error as Error, data: [] });
   }
 };
 
@@ -21,10 +37,28 @@ export const saveNewModel = async (modelData: Model) => {
   try {
     const response = await httpInstance.post("/models", modelData);
     if (response.status === 200 && response.data) {
-      return { success: true, data: response.data };
+      return createResponse({ success: true, data: response.data });
     }
-    return { success: false, data: response.data.message || "Model not found" };
+    return createResponse({
+      success: false,
+      message: response.data.message || "Error saving model",
+    });
   } catch (error) {
-    return { success: false, error: error };
+    createResponse({ success: false, message: error as Error });
+  }
+};
+
+export const updateModel = async (modelData: Model) => {
+  try {
+    const response = await httpInstance.put("/models", modelData);
+    if (response.status === 200 && response.data) {
+      return createResponse({ success: true, data: response.data });
+    }
+    return createResponse({
+      success: false,
+      message: response.data.message || "Error updating model",
+    });
+  } catch (error) {
+    return createResponse({ success: false, message: error as Error });
   }
 };
