8e9add627a4f78196d259d088c1ea8a704eaf359
Juan David Arias <juasda96@gmail.com>
2023-09-13 00:43:45 -0500

    add random prompt generator

--- morpheus-worker/app/schemas/schemas.py
+++ morpheus-worker/app/schemas/schemas.py
@@ -1,10 +1,11 @@
-import uuid
+import random
 from enum import Enum
 from typing import Optional
 
 from pydantic import BaseModel
 
 from app.settings.settings import get_settings
+from app.utils.prompts import generate_random_prompt
 
 settings = get_settings()
 
@@ -35,21 +36,7 @@ class Prompt(BaseModel):
     user_id: str
 
     class Config:
+        size = random.choice([480, 512, 768, 1024])
         schema_extra = {
-            "example": {
-                "task_id": str(uuid.uuid4()),
-                "prompt": "a beautiful cat with blue eyes, artwork, fujicolor, trending on artstation",
-                "negative_prompt": "bad, low res, ugly, deformed",
-                "width": 768,
-                "height": 768,
-                "num_inference_steps": 50,
-                "guidance_scale": 10,
-                "num_images_per_prompt": 1,
-                "generator": -1,
-                "strength": 0.75,
-                "pipeline": settings.default_pipeline,
-                "scheduler": settings.default_scheduler,
-                "model_id": settings.default_model,
-                "user_id": "ray@morpheus.com",
-            }
+            "example": generate_random_prompt()
         }
