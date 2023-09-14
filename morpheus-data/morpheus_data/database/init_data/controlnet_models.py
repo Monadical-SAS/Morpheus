e9cf4a146232a39cc2f6c8925cd9854f9e8dfe17
César Alfredo Uribe León <cesar@monadical.com>
2023-09-14 12:23:27 -0500

    add more models at db init

--- morpheus-data/morpheus_data/database/init_data/controlnet_models.py
+++ morpheus-data/morpheus_data/database/init_data/controlnet_models.py
@@ -1,5 +1,6 @@
 from morpheus_data.database.init_data.categories import controlnet, processing
 
+
 controlnet_models = [
     {
         "name": "Canny edges",
@@ -9,7 +10,7 @@ controlnet_models = [
         ),
         "source": "lllyasviel/sd-controlnet-canny",
         "url_docs": "https://huggingface.co/lllyasviel/sd-controlnet-canny",
-        "categories": [controlnet, processing ],
+        "categories": [controlnet, processing],
         "extra_params": {"type": "canny"},
     },
     {
@@ -33,5 +34,63 @@ controlnet_models = [
         "url_docs": "https://huggingface.co/lllyasviel/sd-controlnet-seg",
         "categories": [controlnet, processing],
         "extra_params": {"type": "seg"},
+    },
+    {
+        "name": "Soft edges (HED)",
+        "description": (
+            "ControlNet is a neural network structure to control diffusion models by adding extra "
+            "conditions. This checkpoint corresponds to the ControlNet conditioned on HED Boundary."
+        ),
+        "source": "lllyasviel/sd-controlnet-hed",
+        "url_docs": "https://huggingface.co/lllyasviel/sd-controlnet-hed",
+        "categories": [controlnet, processing],
+        "extra_params": {"type": "hed"},
+    },
+    {
+        "name": "Lines (M-LSD)",
+        "description": (
+            "ControlNet is a neural network structure to control diffusion models by adding extra "
+            "conditions. This checkpoint corresponds to the ControlNet conditioned on M-LSD straight line"
+            " detection."
+        ),
+        "source": "lllyasviel/sd-controlnet-mlsd",
+        "url_docs": "https://huggingface.co/lllyasviel/sd-controlnet-mlsd",
+        "categories": [controlnet, processing],
+        "extra_params": {"type": "mlsd"},
+    },
+    {
+        "name": "Normals",
+        "description": (
+            "ControlNet is a neural network structure to control diffusion models by adding extra "
+            "conditions. This checkpoint corresponds to the ControlNet conditioned on Normal Map "
+            "Estimation."
+        ),
+        "source": "lllyasviel/sd-controlnet-normal",
+        "url_docs": "https://huggingface.co/lllyasviel/sd-controlnet-normal",
+        "categories": [controlnet, processing],
+        "extra_params": {"type": "normalmap"},
+    },
+    {
+        "name": "Pose",
+        "description": (
+            "ControlNet is a neural network structure to control diffusion models by adding extra "
+            "conditions. This checkpoint corresponds to the ControlNet conditioned on Human Pose "
+            "Estimation."
+        ),
+        "source": "lllyasviel/sd-controlnet-openpose",
+        "url_docs": "https://huggingface.co/lllyasviel/sd-controlnet-openpose",
+        "categories": [controlnet, processing],
+        "extra_params": {"type": "poses"},
+    },
+    {
+        "name": "Scribbles",
+        "description": (
+            "ControlNet is a neural network structure to control diffusion models by adding extra "
+            "conditions. This checkpoint corresponds to the ControlNet conditioned on Scribble images."
+        ),
+        "source": "lllyasviel/sd-controlnet-scribble",
+        "url_docs": "https://huggingface.co/lllyasviel/sd-controlnet-scribble",
+        "categories": [controlnet, processing],
+        "extra_params": {"type": "scribble"},
     },
 ]
