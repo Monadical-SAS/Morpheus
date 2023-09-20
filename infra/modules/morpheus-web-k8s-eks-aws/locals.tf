76e66efbc2d87d81329c902d57ab654218bee495
Angel <43050896+afreydev@users.noreply.github.com>
2023-09-20 15:06:42 -0500

    chore: Enabling ray in Morpheus (#126)
    * advance
    
    * chore: new env vars in ray and server
    
    ---------
    
    Co-authored-by: Angel Rey <angel@monadical.com>

--- infra/modules/morpheus-web-k8s-eks-aws/locals.tf
+++ infra/modules/morpheus-web-k8s-eks-aws/locals.tf
@@ -14,10 +14,14 @@ locals {
   s3_results_bucket_name         = "${var.s3_results_bucket_name}-${var.env}-${random_integer.suffix.result}"
   cluster_ssh_key                = "${var.cluster_ssh_key_name}-${random_integer.suffix.result}"
   cloudwatch_namespace_name      = "morpheus-${random_integer.suffix.result}"
-  scale_up_policy_name            = "morpheus-scale-up-policy-${random_integer.suffix.result}"
-  scale_down_policy_name          = "morpheus-scale-down-policy-${random_integer.suffix.result}"
+  scale_up_policy_name           = "morpheus-scale-up-policy-${random_integer.suffix.result}"
+  scale_up_adv_policy_name       = "morpheus-scale-up-adv-policy-${random_integer.suffix.result}"
+  scale_down_policy_name         = "morpheus-scale-down-policy-${random_integer.suffix.result}"
+  scale_down_adv_policy_name     = "morpheus-scale-down-adv-policy-${random_integer.suffix.result}"
   scale_up_alarm_name            = "morpheus-scale-up-alarm-${random_integer.suffix.result}"
+  scale_up_adv_alarm_name        = "morpheus-scale-up-adv-alarm-${random_integer.suffix.result}"
   scale_down_alarm_name          = "morpheus-scale-down-alarm-${random_integer.suffix.result}"
+  scale_down_adv_alarm_name      = "morpheus-scale-down-adv-alarm-${random_integer.suffix.result}"
   db_creds = jsondecode(
     data.aws_secretsmanager_secret_version.creds.secret_string
   )
