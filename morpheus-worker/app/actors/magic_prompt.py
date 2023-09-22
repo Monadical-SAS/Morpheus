e1ced51a819e771ea289008426d22646f962cbe5
Juan David Arias <juasda96@gmail.com>
2023-09-22 13:26:36 -0500

    use "WorkerCpu": 1 for magic prompt

--- morpheus-worker/app/actors/magic_prompt.py
+++ morpheus-worker/app/actors/magic_prompt.py
@@ -1,5 +1,6 @@
 import logging
 import random
+from pathlib import Path
 
 import ray
 from app.models.schemas import ModelRequest
@@ -10,14 +11,14 @@ from dynamicprompts.generators.magicprompt import MagicPromptGenerator
 settings = get_settings()
 
 
-@ray.remote(num_cpus=1)
+@ray.remote(resources={"WorkerCpu": 1})
 class StableDiffusionMagicPrompt:
     def __init__(self, model_id: str = "Gustavosta/MagicPrompt-Stable-Diffusion"):
         self.logger = logging.getLogger("ray")
 
         # Get the model source, path for local model, model_id for hugin face remote model
-        # self.local_model_path = Path(settings.models_folder).joinpath(model_id)
-        # self.model_source = self.local_model_path if Path(self.local_model_path).exists() else model_id
+        self.local_model_path = Path(settings.models_folder).joinpath(model_id)
+        self.model_source = self.local_model_path if Path(self.local_model_path).exists() else model_id
 
         self.generator = RandomPromptGenerator()
         self.magic_generator = MagicPromptGenerator(
@@ -26,8 +27,8 @@ class StableDiffusionMagicPrompt:
             device="cpu",
         )
 
-        # if not Path(self.local_model_path).exists():
-        #     self.magic_generator.generator.save_pretrained(save_directory=str(self.local_model_path))
+        if not Path(self.local_model_path).exists():
+            self.magic_generator.generator.save_pretrained(save_directory=str(self.local_model_path))
 
     def generate(self, request: ModelRequest):
         self.logger.info(f"StableDiffusionMagicPrompt.generate: request: {request}")
