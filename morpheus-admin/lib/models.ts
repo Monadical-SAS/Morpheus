c637d46711201eba2cef217a1f295ee59588c805
juanArias8 <juan.david@monadical.com>
2023-09-30 03:38:32 -0500

    add error response models

--- morpheus-admin/lib/models.ts
+++ morpheus-admin/lib/models.ts
@@ -30,3 +30,16 @@ export interface User {
   phone?: string;
   is_active: boolean;
 }
+
+export const ErrorResponse = (
+  message: string = "An unexpected error occurred during the operation",
+) => {
+  return { success: false, message: message, data: null };
+};
+
+export const SuccessResponse = (
+  data: any,
+  message: string = "Operation completed successfully",
+) => {
+  return { success: true, data: data, message: message };
+};
